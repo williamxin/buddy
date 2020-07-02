29109185bb010b9f6a28ca0ed7d986add358cb61
xinyong <xinyong@shandagames.com>
2020-07-02 16:22:34 +0800

    优化目录结构

--- src/buddy.c
+++ src/buddy.c
@@ -1,4 +1,4 @@
-#include "buddy.h"
+﻿#include "buddy.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
@@ -6,7 +6,7 @@
 #include <string.h>
 
 #define NODE_UNUSED 0
-#define NODE_USED 1	
+#define NODE_USED 1
 #define NODE_SPLIT 2
 #define NODE_FULL 3
 
@@ -15,47 +15,37 @@ struct buddy {
 	uint8_t tree[1];
 };
 
-struct buddy * 
-buddy_new(int level) {
+struct buddy* buddy_new(int level) {
 	int size = 1 << level;
-	struct buddy * self = malloc(sizeof(struct buddy) + sizeof(uint8_t) * (size * 2 - 2));
+	struct buddy* self = malloc(sizeof(struct buddy) + sizeof(uint8_t) * (size * 2 - 2));
 	self->level = level;
-	memset(self->tree , NODE_UNUSED , size*2-1);
+	memset(self->tree, NODE_UNUSED, size * 2 - 1);
 	return self;
 }
 
-void
-buddy_delete(struct buddy * self) {
-	free(self);
-}
+void buddy_delete(struct buddy* self) { free(self); }
 
-static inline int
-is_pow_of_2(uint32_t x) {
-	return !(x & (x-1));
-}
+static inline int is_pow_of_2(uint32_t x) { return !(x & (x - 1)); }
 
-static inline uint32_t
-next_pow_of_2(uint32_t x) {
-	if ( is_pow_of_2(x) )
+static inline uint32_t next_pow_of_2(uint32_t x) {
+	if (is_pow_of_2(x))
 		return x;
-	x |= x>>1;
-	x |= x>>2;
-	x |= x>>4;
-	x |= x>>8;
-	x |= x>>16;
-	return x+1;
+	x |= x >> 1;
+	x |= x >> 2;
+	x |= x >> 4;
+	x |= x >> 8;
+	x |= x >> 16;
+	return x + 1;
 }
 
-static inline int
-_index_offset(int index, int level, int max_level) {
+static inline int _index_offset(int index, int level, int max_level) {
 	return ((index + 1) - (1 << level)) << (max_level - level);
 }
 
-static void 
-_mark_parent(struct buddy * self, int index) {
+static void _mark_parent(struct buddy* self, int index) {
 	for (;;) {
 		int buddy = index - 1 + (index & 1) * 2;
-		if (buddy > 0 && (self->tree[buddy] == NODE_USED ||	self->tree[buddy] == NODE_FULL)) {
+		if (buddy > 0 && (self->tree[buddy] == NODE_USED || self->tree[buddy] == NODE_FULL)) {
 			index = (index + 1) / 2 - 1;
 			self->tree[index] = NODE_FULL;
 		} else {
@@ -64,10 +54,9 @@ _mark_parent(struct buddy * self, int index) {
 	}
 }
 
-int 
-buddy_alloc(struct buddy * self , int s) {
+int buddy_alloc(struct buddy* self, int s) {
 	int size;
-	if (s==0) {
+	if (s == 0) {
 		size = 1;
 	} else {
 		size = (int)next_pow_of_2(s);
@@ -96,8 +85,8 @@ buddy_alloc(struct buddy * self , int s) {
 			case NODE_UNUSED:
 				// split first
 				self->tree[index] = NODE_SPLIT;
-				self->tree[index*2+1] = NODE_UNUSED;
-				self->tree[index*2+2] = NODE_UNUSED;
+				self->tree[index * 2 + 1] = NODE_UNUSED;
+				self->tree[index * 2 + 2] = NODE_UNUSED;
 			default:
 				index = index * 2 + 1;
 				length /= 2;
@@ -112,7 +101,7 @@ buddy_alloc(struct buddy * self , int s) {
 		for (;;) {
 			level--;
 			length *= 2;
-			index = (index+1)/2 -1;
+			index = (index + 1) / 2 - 1;
 			if (index < 0)
 				return -1;
 			if (index & 1) {
@@ -125,13 +114,12 @@ buddy_alloc(struct buddy * self , int s) {
 	return -1;
 }
 
-static void 
-_combine(struct buddy * self, int index) {
+static void _combine(struct buddy* self, int index) {
 	for (;;) {
 		int buddy = index - 1 + (index & 1) * 2;
 		if (buddy < 0 || self->tree[buddy] != NODE_UNUSED) {
 			self->tree[index] = NODE_UNUSED;
-			while (((index = (index + 1) / 2 - 1) >= 0) &&  self->tree[index] == NODE_FULL){
+			while (((index = (index + 1) / 2 - 1) >= 0) && self->tree[index] == NODE_FULL) {
 				self->tree[index] = NODE_SPLIT;
 			}
 			return;
@@ -140,9 +128,8 @@ _combine(struct buddy * self, int index) {
 	}
 }
 
-void
-buddy_free(struct buddy * self, int offset) {
-	assert( offset < (1<< self->level));
+void buddy_free(struct buddy* self, int offset) {
+	assert(offset < (1 << self->level));
 	int left = 0;
 	int length = 1 << self->level;
 	int index = 0;
@@ -169,9 +156,8 @@ buddy_free(struct buddy * self, int offset) {
 	}
 }
 
-int
-buddy_size(struct buddy * self, int offset) {
-	assert( offset < (1<< self->level));
+int buddy_size(struct buddy* self, int offset) {
+	assert(offset < (1 << self->level));
 	int left = 0;
 	int length = 1 << self->level;
 	int index = 0;
@@ -197,33 +183,30 @@ buddy_size(struct buddy * self, int offset) {
 	}
 }
 
-static void
-_dump(struct buddy * self, int index , int level) {
+static void _dump(struct buddy* self, int index, int level) {
 	switch (self->tree[index]) {
 	case NODE_UNUSED:
-		printf("(%d:%d)", _index_offset(index, level, self->level) , 1 << (self->level - level));
+		printf("(%d:%d)", _index_offset(index, level, self->level), 1 << (self->level - level));
 		break;
 	case NODE_USED:
-		printf("[%d:%d]", _index_offset(index, level, self->level) , 1 << (self->level - level));
+		printf("[%d:%d]", _index_offset(index, level, self->level), 1 << (self->level - level));
 		break;
 	case NODE_FULL:
 		printf("{");
-		_dump(self, index * 2 + 1 , level+1);
-		_dump(self, index * 2 + 2 , level+1);
+		_dump(self, index * 2 + 1, level + 1);
+		_dump(self, index * 2 + 2, level + 1);
 		printf("}");
 		break;
 	default:
 		printf("(");
-		_dump(self, index * 2 + 1 , level+1);
-		_dump(self, index * 2 + 2 , level+1);
+		_dump(self, index * 2 + 1, level + 1);
+		_dump(self, index * 2 + 2, level + 1);
 		printf(")");
 		break;
 	}
 }
 
-void
-buddy_dump(struct buddy * self) {
-	_dump(self, 0 , 0);
+void buddy_dump(struct buddy* self) {
+	_dump(self, 0, 0);
 	printf("\n");
 }
-
